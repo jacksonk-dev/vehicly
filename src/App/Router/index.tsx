8c46ae557ea8519d5c8971271a3a13b0828d3323
jackson.kamya <jackson.kamya@metropol.co.ug>
2022-12-16 15:57:13 +0300

    Improve app code structure

--- src/App/Router/index.tsx
+++ src/App/Router/index.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import LandingPage from 'App/LandingPage';
+import LandingPage from 'App/Page/LandingPage';
 
 export default createBrowserRouter([
     {
