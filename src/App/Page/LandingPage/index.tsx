4bcb958c6e4663384b9a9642f105ce6833a18f64
jackson.kamya <jackson.kamya@metropol.co.ug>
2023-06-05 10:59:03 +0300

    Add more service buttons

--- src/App/Page/LandingPage/index.tsx
+++ src/App/Page/LandingPage/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, Center, Heading, Text, Box, Image, Button } from "@chakra-ui/react";
+import { Flex, Center, Heading, Text, Box, Image, Button, ScaleFade } from "@chakra-ui/react";
 
 export default function LandingPage(){
+  const year = new Date().getFullYear();
+
   return (
     <Box h='100vh' w='100vw' bgGradient='linear(to-r, #000, red.400)'>
       <Center zIndex='10' w='100%' h='100%' background='inherit' padding='24px'>
         <Flex direction='column' alignItems='center' >
-          <Image src='src/Assets/Images/bg.png' w='800px' h='auto' position='relative' top='-80px' />
+          <ScaleFade initialScale={0} in>
+            <Image src='src/Assets/Images/bg.png' w='800px' h='auto' position='relative' top='-80px' />
+          </ScaleFade>
           <Heading 
             zIndex='10'
             size='4xl'
@@ -25,12 +29,21 @@ export default function LandingPage(){
             textAlign='center'
             position='relative' top='-100px'
           >
-            Vehicle and spare parts dealership, repair and cleaning services.
+            Vehicle and spare parts dealership, repair, training and cleaning services. Make booking below.
           </Text>
           <Flex marginTop='24px' display='grid' gridTemplateColumns='170px 170px' gridGap='8px' position='relative' top='-100px'>
-            <Button padding='24px'>Book Cleaning</Button>
-            <Button padding='24px'>Book Repair</Button>
+            <Flex display='grid' gridTemplateColumns='170px' gridGap='8px'>
+              <Button padding='24px'>Cleaning</Button>
+              <Button padding='24px'>Repair</Button>
+              <Button padding='24px'>Stuck On Road</Button>
+            </Flex>
+            <Flex display='grid' gridTemplateColumns='170px' gridGap='8px'>
+              <Button padding='24px'>Spare Parts</Button>
+              <Button padding='24px'>Buy Bike</Button>
+              <Button padding='24px'>Book Training</Button>
+            </Flex>
           </Flex>
+          <Text color='#fff' position='fixed' bottom='16px'>&copy; Vehicly {year}</Text>
         </Flex>
       </Center>
     </Box>
